4a94e897bf06f824604ea7cdd0c3f87f76ab4f2a
yazzy4 <yazzy4@gmail.com>
2019-12-17 13:33:03 -0500

    About page presenting

--- Bullseye2/AboutViewController.swift
+++ Bullseye2/AboutViewController.swift
@@ -12,9 +12,12 @@ class AboutViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      
     }
    
+    @IBAction func close (){
+         dismiss(animated: true, completion: nil)
+    }
     
 
 
