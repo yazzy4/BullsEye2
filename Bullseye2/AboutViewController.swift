cd1fbd997d6084ae2292fe101fa755a61d311ab3
yazzy4 <yazzy4@gmail.com>
2019-12-18 11:09:57 -0500

    Added a webkit view to about page

--- Bullseye2/AboutViewController.swift
+++ Bullseye2/AboutViewController.swift
@@ -5,18 +5,23 @@
 //  Created by Yaz Burrell on 12/17/19.
 //  Copyright © 2019 Yaz Burrell. All rights reserved.
 //
-
+import WebKit
 import UIKit
 
 class AboutViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      
+    
+        override func viewDidLoad() { super.viewDidLoad()
+            
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            let request = URLRequest(url: url)
+        webView.load(request) }
     }
-   
+
+    @IBOutlet weak var webView: WKWebView!
+    
     @IBAction func close (){
          dismiss(animated: true, completion: nil)
+        
     }
     
 
